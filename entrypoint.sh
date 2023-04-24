aad71b3484ea9dd46eeceec7d0ed6a74f5e1cd50
Abdelhay.Ali <Abdelhay@eng.aun.edu.eg>
2023-04-24 12:08:19 -0400

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -108,8 +108,8 @@ set_output elapsed_time "$(echo "$(date +%s)"-"$start_time" | bc)"
 msg "Packaging the kernel..."
 
     
-tools/mkbootimg --kernel out2/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --base 0x00478000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x0ff88000 --second_offset 0x00e88000 --tags_offset 0x07988000 --os_version 9.0.0 --os_patch_level 2020-01 --header_version 0 --output Kernel-4.9.148-KSU-enforcing-${date}.img
+tools/mkbootimg --kernel out2/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --base 0x00478000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x0ff88000 --second_offset 0x00e88000 --tags_offset 0x07988000 --os_version 9.0.0 --os_patch_level 2020-01 --header_version 0 --output Kernel-4.9.319-KSU-enforcing-${date}.img
 
-set_output outfile Kernel-4.9.148-KSU-enforcing-${date}.img
+set_output outfile Kernel-4.9.319-KSU-enforcing-${date}.img
 
     exit 0
